c7276e1d3ea8dd37f034e5e5afddc07792cbeffc
Arnout Engelen <arnout@bzzt.net>
2024-02-08 10:50:22 +0100

    list which projects are known to publish sboms

---
+++ collect-all-latest.sh
@@ -0,0 +1,16 @@
+python3 ./collect-airflow-sbom.py
+./collect-maven-sboms.py
+./collect-maven-sboms.py arrow
+./collect-maven-sboms.py avro
+./collect-maven-sboms.py druid
+# TODO handle weird versions
+./collect-maven-sboms.py flink
+# TODO hadoop: nothing found?
+# TODO hive: not yet released?
+./collect-maven-sboms.py hbase
+# TODO handle weird versions
+./collect-maven-sboms.py maven
+./collect-maven-sboms.py orc
+./collect-maven-sboms.py parquet
+./collect-maven-sboms.py syncope
+./collect-maven-sboms.py zookeeper
