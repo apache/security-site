ad8933506c257cb6002f456afbc5c3814230c09a
Arnout Engelen <arnout@bzzt.net>
2024-06-28 09:03:46 +0200

    Remove some exceptions
    Upstream fixes were merged

--- scripts/check-coordinates-with-doap.py
+++ scripts/check-coordinates-with-doap.py
@@ -20,7 +20,11 @@ def fetch_doap(url):
           d.write(f.read().decode('utf-8'))
     with open(filename, 'r') as d:
         f = Graph()
-        f.parse(data=d.read(), format='xml')
+        try:
+            f.parse(data=d.read(), format='xml')
+        except Exception:
+            print(f"Error parsing {url} ({filename}))")
+            raise
         return f
 
 # manually maintained
@@ -41,23 +45,12 @@ class MYDOAP(DefinedNamespace):
     _NS = Namespace("http://usefulinc.com/ns/doap#")
 
 for location in ET.parse('projects.xml').getroot():
-    #print(location.text)
-    # https://github.com/apache/cordova-docs/pull/1353
-    if (location.text == "https://cordova.apache.org/infra/doap_Cordova.rdf" or 
-
-        # https://github.com/apache/doris/pull/36956
-        location.text == "https://gitbox.apache.org/repos/asf?p=doris.git;a=blob_plain;f=doap_Doris.rdf;hb=HEAD" or
-        # https://github.com/apache/kafka/pull/16472
+    if (# https://github.com/apache/kafka/pull/16472
         location.text == "https://gitbox.apache.org/repos/asf?p=kafka.git;a=blob_plain;f=doap_Kafka.rdf;hb=HEAD" or
         # https://github.com/apache/olingo-site/pull/6
         location.text == "http://olingo.apache.org/doap_Olingo.rdf" or
         # https://github.com/apache/orc/pull/1964
         location.text == "http://orc.apache.org/doap_orc.rdf" or
-        # https://github.com/apache/sis-site/pull/3
-        location.text == "http://sis.apache.org/DOAP.rdf" or
-        # https://github.com/apache/yunikorn-site/pull/444
-        location.text == "https://gitbox.apache.org/repos/asf?p=yunikorn-site.git;a=blob_plain;f=doap_YuniKorn.rdf;hb=HEAD" or
-
         # https://github.com/apache/kibble-website/pull/15
         location.text == "https://kibble.apache.org/doap.rdf" or
         # https://github.com/apache/hudi/pull/11533
