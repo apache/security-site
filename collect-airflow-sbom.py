e8d6108360d213d5187e3cc412d8eda1015743d5
Arnout Engelen <arnout@bzzt.net>
2024-07-31 20:17:50 +0200

    add all current airflow sboms

--- collect-airflow-sbom.py
+++ collect-airflow-sbom.py
@@ -9,15 +9,13 @@ baseurl = "https://airflow.apache.org/docs/apache-airflow/stable/sbom/"
 
 files = list(map(lambda e: e['href'], get_files(baseurl)))
 files.sort()
-file = files[0]
 
-match = re.search('apache-airflow-sbom-(.*?)-(.*).json', file)
-version = match.group(1)
-variant = match.group(2)
+for file in files:
+    match = re.search('apache-airflow-sbom-(.*?)-(.*).json', file)
+    version = match.group(1)
+    variant = match.group(2)
 
-pmc = dt_pmc("airflow")
-pmc_uuid = pmc['uuid']
-sbom = get_sbom_cached(f'{baseurl}/{file}', f"airflow/{file}")
-post_sbom(pmc, pmc_uuid, f"Apache Airflow {variant}", version, sbom)
-
-
+    pmc = dt_pmc("airflow")
+    pmc_uuid = pmc['uuid']
+    sbom = get_sbom_cached(f'{baseurl}/{file}', f"airflow/{file}")
+    post_sbom(pmc, pmc_uuid, f"Apache Airflow {variant}", version, sbom)
