45372f28b274b623709dca15d436d4fc92513652
Arnout Engelen <arnout@bzzt.net>
2024-11-07 16:21:54 +0100

    app/version-diff: allow non-sboms in the sbom folder for now

--- app/scripts/api/artifacts.py
+++ app/scripts/api/artifacts.py
@@ -5,11 +5,11 @@ import os
 
 async def process(state: typing.Any, request, formdata: dict) -> typing.Any:
     artifacts = []
-    for (root, _, files) in os.walk("./sboms"):
+    for (root, _, files) in os.walk("./sboms/"):
         if len(files) != 0:
             pmc = root.split('/')[2]
             artifact = '/'.join(root.split('/')[3:-1])
-            if not (pmc, artifact) in artifacts:
+            if artifact and not (pmc, artifact) in artifacts:
                 artifacts.append((pmc, artifact))
     return artifacts
  
