646a1d098887af41deba46645f862eeb6082e16f
Hervé Boutemy <hboutemy@apache.org>
2024-11-08 08:32:42 +0100

    refactor code to describe releases

---
+++ sboms/logging/log4j-2.23.1.md
@@ -0,0 +1,8 @@
+Log4j 2.23.1: 3 SBOMs
+=======
+
+| file, spec<br>Serial Number, version| metadata | components<br>by type<br>- libs purl types |
+| ----------------------------------- | -------- | ------------------------------------------ |
+| **[log4j-flume-ng-2.23.1-cyclonedx](maven/org.apache.logging.log4j/log4j-flume-ng/2.23.1/log4j-flume-ng-2.23.1-cyclonedx.xml)**<br>xml 1.5<br>urn:uuid:d416c147-b953-3a15-8ddc-6022bf29dc22<br>version: 1 | **maven/org.apache.logging.log4j/log4j-flume-ng@2.23.1?type=jar**<br>type: LIBRARY<br>timestamp: 2024-11-08 08:28:50<br>tool: CycloneDX Maven plugin 2.7.10 | 116<br>`library`: 116 <br>- `maven`: 116  |
+| **[log4j-kubernetes-2.23.1-cyclonedx](maven/org.apache.logging.log4j/log4j-kubernetes/2.23.1/log4j-kubernetes-2.23.1-cyclonedx.xml)**<br>xml 1.5<br>urn:uuid:1c1af6c2-2f68-3635-9088-b2c5b75fab20<br>version: 1 | **maven/org.apache.logging.log4j/log4j-kubernetes@2.23.1?type=jar**<br>type: LIBRARY<br>timestamp: 2024-11-08 08:28:50<br>tool: CycloneDX Maven plugin 2.7.10 | 45<br>`library`: 45 <br>- `maven`: 45  |
+| **[log4j-mongodb3-2.23.1-cyclonedx](maven/org.apache.logging.log4j/log4j-mongodb3/2.23.1/log4j-mongodb3-2.23.1-cyclonedx.xml)**<br>xml 1.5<br>urn:uuid:e10f7432-f5bf-325a-918e-c75eec944af9<br>version: 1 | **maven/org.apache.logging.log4j/log4j-mongodb3@2.23.1?type=jar**<br>type: LIBRARY<br>timestamp: 2024-11-08 08:28:50<br>tool: CycloneDX Maven plugin 2.7.10 | 15<br>`library`: 15 <br>- `maven`: 15  |
--- scripts/describe-sboms.sh
+++ scripts/describe-sboms.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/zsh
 
 # look at SBOMs in sboms/{pmc} and generate release SBOMs description in sboms/{pmc}/{project}-{version}.md
 # that lists the SBOMs associated to the release, and for each a high-level summary:
@@ -7,88 +7,48 @@
 
 describe="$(pwd)/scripts/describe_sboms.java"
 
-echo "> describing Airflow SBOMs"
-cd sboms/airflow
-for d in */apache-airflow/2.*
-do
-  version=$(basename $d)
-  echo "  > describing Airflow $version"
-  $describe Airflow $version $d/*.json > airflow-$version.md
-done
-cd ../..
+function describeReleasesFromBasedir() {
+  local pmc=$1
+  local name=$2
+  local basedir=$3
 
-echo "> describing Flink SBOMs"
-cd sboms/flink
-for d in */org.apache.flink/flink-core/*
-do
-  version=$(basename $d)
-  echo "  > describing Flink $version"
-  $describe Flink $version */org.apache.flink/flink*/$version/*.json > flink-$version.md
-done
-cd ../..
+  echo "> describing $name SBOMs"
+  cd sboms/$pmc
+  for d in */$basedir/*
+  do
+    local version=$(basename $d)
+    echo "  > describing $name $version"
+    $describe $name $version $d/*.(xml|json) > $pmc-$version.md
+  done
+  cd ../..
+}
 
-echo "> describing Groovy SBOMs"
-cd sboms/groovy
-for d in */org.apache.groovy/groovy/*
-do
-  version=$(basename $d)
-  echo "  > describing Groovy $version"
-  $describe Groovy $version */org.apache.groovy/groovy*/$version/*.json > groovy-$version.md
-done
-cd ../..
+function describeReleasesFromBase() {
+  local pmc=$1
+  local name=$2
+  local id=$3
+  local baseDir=$4
+  local baseSub=$5
+  [ "$id" = "-" ] && id=$pmc
 
-echo "> describing JSPWiki SBOMs"
-cd sboms/jspwiki
-for d in */org.apache.jspwiki/jspwiki-war/2.*
-do
-  version=$(basename $d)
-  echo "  > describing JSPWiki $version"
-  $describe JSPWiki $version */org.apache.jspwiki*/jspwiki-*/$version/*.xml > jspwiki-$version.md
-done
-cd ../..
+  echo "> describing $name SBOMs"
+  cd sboms/$pmc
+  local version
+  for version in $(echo "$(for d in */$baseDir*/$baseSub*/* ; do filename $d ; done)" | sort -u)
+  do
+    echo "  > describing $name $version"
+    $describe $name $version */$baseDir*/$baseSub*/$version/*.(xml|json) > $id-$version.md
+  done
+  cd ../..
+}
 
-echo "> describing Logging SBOMs"
-cd sboms/logging
-for d in */org.apache.logging.log4j/log4j-api/*
-do
-  version=$(basename $d)
-  echo "  > describing Log4j $version"
-  $describe Log4j $version */org.apache.logging.log4j/log4j-*/$version/*.xml > log4j-$version.md
-done
-cd ../..
+describeReleasesFromBasedir airflow Airflow apache-airflow
 
-echo "> describing Pekko SBOMs"
-cd sboms/pekko
-for d in */org.apache.pekko/pekko-bom_2.13/*
-do
-  version=$(basename $d)
-  echo "  > describing Pekko $version"
-  $describe Pekko $version */org.apache.pekko/pekko-*/$version/*.xml > pekko-$version.md
-done
-cd ../..
-
-echo "> describing Turbine SBOMs"
-cd sboms/turbine
-for d in */org.apache.turbine/turbine/*
-do
-  version=$(basename $d)
-  echo "  > describing Turbine $version"
-  $describe Turbine $version */org.apache.turbine/turbine*/$version/*.json > turbine-$version.md
-done
-for d in */org.apache.fulcrum/fulcrum-security/*
-do
-  version=$(basename $d)
-  echo "  > describing Fulcrum $version"
-  $describe Fulcrum $version */org.apache.fulcrum/fulcrum*/$version/*.json > fulcrum-$version.md
-done
-cd ../..
-
-echo "> describing Zookeeper SBOMs"
-cd sboms/zookeeper
-for d in */org.apache.zookeeper/zookeeper-jute/*
-do
-  version=$(basename $d)
-  echo "  > describing Zookeeper $version"
-  $describe Zookeeper $version */org.apache.zookeeper/zookeeper*/$version/*.json > zookeeper-$version.md
-done
-cd ../..
+describeReleasesFromBase flink Flink - org.apache.flink flink
+describeReleasesFromBase groovy Groovy - org.apache.groovy groovy
+describeReleasesFromBase jspwiki JSPWiki - org.apache.jspwiki jspwiki
+describeReleasesFromBase logging Log4j log4j org.apache.logging.log4j log4j
+describeReleasesFromBase pekko Pekko - org.apache.pekko pekko
+describeReleasesFromBase turbine Turbine - org.apache.turbine turbine
+describeReleasesFromBase turbine Fulcrum fulcrum org.apache.fulcrum fulcrum
+describeReleasesFromBase zookeeper Zookeeper - org.apache.zookeeper zookeeper
