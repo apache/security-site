bdffdf6339c1352a89b3f24605b93d800663e9c2
Arnout Engelen <arnout@bzzt.net>
2026-05-15 17:05:04 +0200

    label renames: fix option usage

--- security-dashboard-backend-watch-label-renames.py
+++ security-dashboard-backend-watch-label-renames.py
@@ -1,5 +1,6 @@
 from gmail_gcloud_subscriber import gmail_subscribe
 from gmail_label_cache import refresh_label_cache, validate_label_name
+from optparse import OptionParser
 import os
 import threading
 from time import sleep
@@ -9,24 +10,22 @@ import subprocess
 # applies the corresponding file renames.
 
 parser = OptionParser()
-parser.add_option("-t", "--target", help="Directory to populate")
+parser.add_option("-t", "--target", default="email-classification/", help="Directory to populate")
 (options, args) = parser.parse_args()
-
-target_directory = options['target'] or "email-classification/"
 
 def label_updated(labelId, old, new):
     validate_label_name(old)
     validate_label_name(new)
-    if os.path.isfile(f"{target_directory}/{old}.json"):
-        if os.path.isfile(f"{target_directory}/{new}.json"):
+    if os.path.isfile(f"{options.target}/{old}.json"):
+        if os.path.isfile(f"{options.target}/{new}.json"):
             # likely merged
-            subprocess.run(["git", "rm", "-f", f"{old}.json"], cwd=target_directory)
+            subprocess.run(["git", "rm", "-f", f"{old}.json"], cwd=options.target)
         else:
-            os.makedirs(target_directory + '/'.join(new.split('/')[:-1]), exist_ok=True)
-            result = subprocess.run(["git", "mv", f"{old}.json", f"{new}.json"], cwd=target_directory)
+            os.makedirs(options.target + '/'.join(new.split('/')[:-1]), exist_ok=True)
+            result = subprocess.run(["git", "mv", f"{old}.json", f"{new}.json"], cwd=options.target)
             if result.returncode != 0:
                 print(f"Move failed (status code {result.returncode}), just removing")
-                os.remove(f"{target_directory}/{old}.json")
+                os.remove(f"{options.target}/{old}.json")
 
 
 def poll_label_changes(interval=10):
